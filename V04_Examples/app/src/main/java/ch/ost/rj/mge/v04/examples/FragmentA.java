8b2675be5d13926d20c34460a6b836bcb413ef8c
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-08-20 14:44:40 +0200

    Android - V04: Beispiele aktualisiert.

--- V04_Examples/app/src/main/java/ch/ost/rj/mge/v04/examples/FragmentA.java
+++ V04_Examples/app/src/main/java/ch/ost/rj/mge/v04/examples/FragmentA.java
@@ -3,11 +3,10 @@ package ch.ost.rj.mge.v04.examples;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class FragmentA extends Fragment {
@@ -27,14 +26,18 @@ public class FragmentA extends Fragment {
         return fragment;
     }
 
+    public FragmentA() {
+        super(R.layout.fragment_a);
+    }
+
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(@NonNull Context context) {
         super.onAttach(context);
 
         try {
             callback = (FragmentCallback) context;
         } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString() + " must implement " + FragmentCallback.class.getSimpleName());
+            throw new ClassCastException(context + " must implement " + FragmentCallback.class.getSimpleName());
         }
     }
 
@@ -48,13 +51,9 @@ public class FragmentA extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View fragment = inflater.inflate(R.layout.fragment_a, container, false);
-
-        textView = fragment.findViewById(R.id.textview_in_first_fragment);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        textView = view.findViewById(R.id.textview_in_first_fragment);
         textView.setOnClickListener(v -> callback.showMessage(textView.getText().toString()));
-
-        return fragment;
     }
 
     @Override
